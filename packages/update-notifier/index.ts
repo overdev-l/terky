c9f75357c349e679cc8b7207610c80920cbf338d
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-04-06 09:44:18 +0800

    feat(update-notifer): 增加loaded事件及页面hide事件更新
    增加loaded事件及页面hide事件更新

--- packages/update-notifier/index.ts
+++ packages/update-notifier/index.ts
@@ -1,21 +1,60 @@
 
 interface Initial {
-  delay: number,
-  url: string,
-  method: string,
-  loop: boolean,
+  delay: number
+  rootPath?: string
+  loop: boolean
+  request?: (toggle:() => void) => void
 }
 
 export function useNotification(params: Initial) {
-  const useCreateNotif = (notice: boolean) => new CustomEvent("siteUpdate", {
-    bubbles: true,
-    detail: { data: notice }
-  })
+  const currentHash = getCurrentHash()
+  if (!currentHash) return
+  init(currentHash)
 }
+
+const useCreateNotify = (notice: boolean) => new CustomEvent("siteUpdate", {
+  bubbles: true,
+  detail: { data: notice }
+})
 
 const getCurrentHash = () => {
   const body = document.querySelector('body')
-  if (!body) return
+  if (!body) return false
   const hash = body.getAttribute('data-hash')
   return hash
+}
+
+const queryNewHash = async () => {
+  const res = await fetch('/hash')
+  const data = await res.json()
+  return data.hash
+}
+
+const init = (currentHash: string) => {
+  window.addEventListener('load', windowLoaded.bind(null, currentHash))
+
+  document.addEventListener('visibilitychange', handleVisibilityChange.bind(null, currentHash))
+}
+
+
+const handleVisibilityChange = async (currentHash: string) => {
+  if (document.visibilityState === 'visible') {
+    const hash = await queryNewHash()
+  if (hash !== currentHash) {
+    const notice = useCreateNotify(true)
+    window.dispatchEvent(notice)
+  }
+ } 
+}
+const windowLoaded = async (currentHash: string) => {
+  const hash = await queryNewHash()
+  if (hash !== currentHash) {
+    const notice = useCreateNotify(true)
+    window.dispatchEvent(notice)
+  }
+}
+
+const interval = (fn: () => void, delay: number) => {
+  fn()
+  return setInterval(fn, delay)
 }
